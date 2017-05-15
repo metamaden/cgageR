eba4da95bdcd7a729555a1c606f23ab0cb8ee509
smaden <metamaden@users.noreply.github.com>
2017-05-15 09:45:45 -0700

    Keep CpGs option
    Remove keeptxt, replace with keepcpgs.epitoc.

--- R/getEpiTOC.R
+++ R/getEpiTOC.R
@@ -1,15 +1,14 @@
-getEpiTOC <- function(df,keeptxt=TRUE){
+getEpiTOC <- function(df,keepcgs.epitoc){
   common.v <- intersect(rownames(df),EpiTOCcpgs);
   map.idx <- match(common.v,rownames(df));
   return.df <- data.frame(EpiTOC.Est=colMeans(df[map.idx,]));
   return.list <- list(return.df); names(return.list) <- "Epitoc.Est"
+  
+  message("Number of available epiTOC CpGs: ",length(common.v),"/",length(EpiTOCcpgs),".",sep="")
 
-  if(keeptxt){
-    txtepitoc <- paste("Number of available epiTOC CpGs: ",length(common.v),"/",length(EpiTOCcpgs),".",sep="")
-    print(txtepitoc);
-    return.list <- append(return.list,txtepitoc)
-    names(return.list)[[length(return.list)]] <- "info"
+  if(keepcpgs.epitoc){
+    return.list <- append(return.list,common.v)
+    names(return.list)[[length(return.list)]] <- "Epitoc.CpGs.Used"
   }
-
   return(return.list)
 }
